42cf4ea3df17b6d2215aa62999a3b12ed018af9e
Roshan Sageflick <roshanneupane600@gmail.com>
2026-06-25 18:27:31 +0545

    feat:: superform implementation in progress

--- src/routes/(auth)/register/+page.server.ts
+++ src/routes/(auth)/register/+page.server.ts
@@ -4,57 +4,41 @@ import { fail } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 import { PostMethod } from '$lib/constants/methods';
 import { RegisterApi } from '$lib/constants/endpoints';
-import { superValidate } from 'sveltekit-superforms';
-import { zod4 } from "sveltekit-superforms/adapters";
+import { superValidate, message } from 'sveltekit-superforms';
+import { zod4 } from 'sveltekit-superforms/adapters';
 
 export const load: PageServerLoad = async () => {
-  return {
-    form: await superValidate(zod4(RegisterSchema)),
-  };
+	return {
+		form: await superValidate(zod4(RegisterSchema))
+	};
 };
-
 
 export const actions = {
 	default: async ({ request, fetch }) => {
 		try {
-			console.log('first')
 			const form = await superValidate(request, zod4(RegisterSchema));
-			console.log('form', form)
-			if(!form.valid){
-				return fail(400, { form })
+			if (!form.valid) {
+				return fail(400, { form });
 			}
-			// const requestData = await request.formData();
+			console.log('first');
 
-			// const formData = {
-			// 	full_name: requestData.get('full_name'),
-			// 	email: requestData.get('email'),
-			// 	password: requestData.get('password'),
-			// 	confirm_password: requestData.get('confirm_password')
-			// };
-
-			// const validatedData = RegisterSchema.safeParse(formData);
-			// if (!validatedData.success) {
-			// 	return fail(400, {
-			// 		errors: formatErrors(validatedData.error)
-			// 	});
-			// }
-
-			// const res = await PostMethod<RegisterFormData, unknown>(
-			// 	RegisterApi,
-			// 	validatedData.data,
-			// 	fetch
-			// );
-			// if (res.status === 201) {
-			// 	return {
-			// 		message: res.message,
-			// 		success: true
-			// 	};
-			// } else {
-			// 	return fail(400, {
-			// 		message: res.message,
-			// 		success: false
-			// 	});
-			// }
+			const res = await PostMethod<RegisterFormData, unknown>(RegisterApi, form.data, fetch);
+			console.log(res);
+			if (res.status === 201) {
+				return message(form, {
+					text: res.message,
+					success: true
+				});
+			} else {
+				return message(
+					form,
+					{
+						text: res.message,
+						success: false
+					},
+					{ status: 400 }
+				);
+			}
 		} catch (error) {
 			return fail(400, {
 				message: 'Something went wrong!',
--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import { goto } from '$app/navigation';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card';
@@ -15,6 +14,7 @@
 		type RegisterFormData
 	} from '$lib/services/register/register.validation';
 	import * as Form from '$lib/components/ui/form/index.js';
+	import { untrack } from 'svelte';
 
 	// let { form }: PageProps = $props();
 	let { data }: PageProps = $props();
@@ -25,11 +25,7 @@
 		validators: zod4Client(RegisterSchema)
 	});
 
-	const { form: formData, errors } = form;
-	$effect(() => {
-		console.log('formData', $formData);
-		console.log('errors', $errors);
-	});
+	const { form: formData, errors, message, enhance } = form;
 	// $effect(() => {
 	// 	if (form?.success) {
 	// 		toast.success(form.message);
@@ -51,64 +47,59 @@
 			<Card.Title class="text-2xl font-bold">Create an account</Card.Title>
 		</Card.Header>
 		<Card.Content>
-			{#if form?.message}
+			{#if $message?.text}
 				<div class="bg-destructive/10 text-destructive mb-4 rounded-md p-3 text-sm">
-					{form.message}
+					{$message?.text}
 				</div>
 			{/if}
 			<form method="POST" use:enhance class="space-y-4">
-				<!-- <Form.Field {form} name="email">
+				<Form.Field {form} name="full_name">
 					<Form.Control>
-						{#snippet children({ props })}
-							<Form.Label>Email</Form.Label>
-							<Input {...props} bind:value={$formData.email} />
-						{/snippet}
+						<Form.Label>Full Name</Form.Label>
+						<Input
+							type="text"
+							placeholder="John Doe"
+							required
+							autocomplete="name"
+						/>
 					</Form.Control>
 					<Form.FieldErrors />
-				</Form.Field> -->
-				<div class="space-y-2">
-					<Label for="name">Full Name</Label>
-					<Input
-						id="full_name"
-						name="full_name"
-						type="text"
-						placeholder="John Doe"
-						required
-						autocomplete="name"
-					/>
-				</div>
-				<div class="space-y-2">
-					<Label for="email">Email</Label>
-					<Input
-						id="email"
-						name="email"
-						type="email"
-						placeholder="john@example.com"
-						required
-						autocomplete="email"
-					/>
-				</div>
-				<div class="space-y-2">
-					<Label for="password">Password</Label>
-					<Input
-						id="password"
-						name="password"
-						type="password"
-						placeholder="8+ characters"
-						required
-						autocomplete="new-password"
-					/>
-				</div>
-				<div class="space-y-2">
-					<Label for="confirm_password">Confirm Password</Label>
-					<Input
-						id="confirm_password"
-						name="confirm_password"
-						type="password"
-						placeholder="Re-enter your password"
-						required
-					/>
-				</div>
+				</Form.Field>
+				<Form.Field {form} name="email">
+					<Form.Control>
+						<Form.Label>Email</Form.Label>
+						<Input
+							placeholder="john@example.com"
+							autocomplete="email"
+							type="email"
+							required
+						/>
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+				<Form.Field {form} name="password">
+					<Form.Control>
+						<Form.Label>Password</Form.Label>
+						<Input
+							type="password"
+							placeholder="6+ characters"
+							required
+							autocomplete="new-password"
+						/>
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
+				<Form.Field {form} name="password">
+					<Form.Control>
+						<Form.Label>Confirm Password</Form.Label>
+						<Input
+							type="password"
+							placeholder="Re-enter your password"
+							required
+						/>
+					</Form.Control>
+					<Form.FieldErrors />
+				</Form.Field>
 				<Button type="submit" class="w-full">Create account</Button>
 			</form>
 		</Card.Content>
