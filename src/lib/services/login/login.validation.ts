f58d1fdd5a704d797f1933a31999bdee400f194e
roshan-neupaney <roshanneupane600@gmail.com>
2026-07-05 19:14:40 +0545

    add password validation for login

--- src/lib/services/login/login.validation.ts
+++ src/lib/services/login/login.validation.ts
@@ -1,8 +1,16 @@
 import { z } from 'zod';
 
+const passwordSchema = z
+  .string()
+  .min(6, { message: "Password must be at least 6 characters long" })
+  .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+  .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
+  .regex(/[0-9]/, { message: "Password must contain at least one number" })
+  .regex(/[^a-zA-Z0-9]/, { message: "Password must contain at least one special character" });
+
 export const loginSchema = z.object({
 	email: z.email('Invalid email format'),
-	password: z.string().min(6, 'Password must be atleast 8 characters')
+	password: passwordSchema
 });
 
 export type LoginFormData = z.infer<typeof loginSchema>;
--- src/lib/services/register/register.validation.ts
+++ src/lib/services/register/register.validation.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 const passwordSchema = z
   .string()
-  .min(6, { message: "Password must be at least 8 characters long" })
+  .min(6, { message: "Password must be at least 6 characters long" })
   .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
   .regex(/[a-z]/, { message: "Password must contain at least one lowercase letter" })
   .regex(/[0-9]/, { message: "Password must contain at least one number" })
