f60e43171dd1772dafd8d6f0495fac25f6ae9a3e
roshan-neupaney <roshanneupane600@gmail.com>
2026-07-07 21:37:54 +0545

    fix output dir build error

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vitest/config';
 import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
 import path from 'node:path';
 
 export default defineConfig({
@@ -23,7 +24,8 @@ export default defineConfig({
 				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			}
+			},
+			adapter: adapter()
 		})
 	],
 	test: {
