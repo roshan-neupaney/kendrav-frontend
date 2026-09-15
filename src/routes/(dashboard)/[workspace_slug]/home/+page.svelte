591ed76421fa4d60f8e4c60d19a5e030a29c2831
Roshan Sageflick <roshanneupane600@gmail.com>
2026-09-15 18:26:31 +0545

    feat:: facebook oauth implementation inprogress

--- src/routes/(dashboard)/[workspace_slug]/home/+page.svelte
+++ src/routes/(dashboard)/[workspace_slug]/home/+page.svelte
@@ -1,8 +1,17 @@
-<script>
-	import { page } from "$app/state";
+<script lang="ts">
+	import { page } from '$app/state';
 
-    const {userData} = page.data;
-    console.log('userData', userData)
+	const { userData } = page.data;
+	
+	console.log('userData', userData);
+    const callbackUrl = 'http://localhost:5173/channel/facebook/callback'
 </script>
+
 <div>home</div>
 <a href="/pasdf/create">Create</a>
+
+<button onclick={() => {
+    window.location.href = `https://www.facebook.com/v26.0/dialog/oauth?client_id=957260226693711&redirect_uri=${encodeURIComponent(callbackUrl)}&scope=pages_read_engagement&pages_messaging&response_type=code`
+}}>
+    Connect Facebook
+</button>
